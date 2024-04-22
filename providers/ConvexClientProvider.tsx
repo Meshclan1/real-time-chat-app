bc35b644c7b04aea3b809b6ae8c2218b8419c86c
Ayo <Ayo.meshe@gmail.com>
2024-04-22 16:54:14 +0100

    integrating clerk with convex

---
+++ providers/ConvexClientProvider.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import React from "react";
+
+type Props = { children: React.ReactNode };
+
+const CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL || "";
+
+const convex = new ConvexReactClient(CONVEX_URL);
+
+const ConvexClientProvider = ({ children }: Props) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
+
+export default ConvexClientProvider;
