57d884f0f8aac63a9a5d4370e2c579cda6a02b35
Ayo <Ayo.meshe@gmail.com>
2024-04-29 21:25:30 +0100

    app update

--- app/(root)/friends/_components/Request.tsx
+++ app/(root)/friends/_components/Request.tsx
@@ -22,6 +22,9 @@ const Request = ({ id, imageUrl, username, email }: Props) => {
   const { mutate: denyRequest, pending: denyPending } = useMutationState(
     api.request.deny
   );
+  const { mutate: acceptRequest, pending: acceptPending } = useMutationState(
+    api.request.accept
+  );
 
   return (
     <Card className="w-full p-2 flex flex-row items-center justify-between gap-2">
@@ -38,13 +41,29 @@ const Request = ({ id, imageUrl, username, email }: Props) => {
         </div>
       </div>
       <div className="flex items-center gap-2">
-        <Button size="icon" disabled={denyPending} onClick={() => {}}>
+        <Button
+          size="icon"
+          disabled={denyPending || acceptPending}
+          onClick={() => {
+            acceptRequest({ id })
+              .then(() => {
+                toast.success("Friend request accepted");
+              })
+              .catch((error) => {
+                toast.error(
+                  error instanceof ConvexError
+                    ? error.data
+                    : "Unexpected error occured"
+                );
+              });
+          }}
+        >
           <Check />
         </Button>
         <Button
           size="icon"
           variant="destructive"
-          disabled={denyPending}
+          disabled={denyPending || acceptPending}
           onClick={() => {
             denyRequest({ id })
               .then(() => {
--- convex/request.ts
+++ convex/request.ts
@@ -3,6 +3,8 @@
 import { mutation } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 import { getUserByClerkId } from "./_utils";
+import { currentUser } from "@clerk/nextjs/dist/types/server";
+import { request } from "http";
 
 export const create = mutation({
   args: {
@@ -56,6 +58,24 @@ export const create = mutation({
     if (requestAlreadyReceived) {
       throw new ConvexError("This user has already sent you a request");
     }
+
+    const friends1 = await ctx.db
+      .query("friends")
+      .withIndex("by_user1", (q) => q.eq("user1", currentUser._id))
+      .collect();
+
+    const friends2 = await ctx.db
+      .query("friends")
+      .withIndex("by_user2", (q) => q.eq("user2", currentUser._id))
+      .collect();
+
+    if (
+      friends1.some((friend) => friend.user2 === receiver._id) ||
+      friends2.some((friend) => friend.user1 === receiver._id)
+    ) {
+      throw new ConvexError("You are already friends with this user");
+    }
+
     const request = await ctx.db.insert("requests", {
       sender: currentUser._id,
       receiver: receiver._id,
@@ -88,6 +108,50 @@ export const deny = mutation({
     if (!request || request.receiver !== currentUser._id) {
       throw new ConvexError("There was an error denyig this request");
     }
+    await ctx.db.delete(request._id);
+  },
+});
+
+export const accept = mutation({
+  args: {
+    id: v.id("requests"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorised");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+    const request = await ctx.db.get(args.id);
+
+    if (!request || request.receiver !== currentUser._id) {
+      throw new ConvexError("There was an error accepting this request");
+    }
+    const conversationId = await ctx.db.insert("conversations", {
+      isGroup: false,
+    });
+
+    await ctx.db.insert("friends", {
+      user1: currentUser._id,
+      user2: request.sender,
+      conversationId,
+    });
+    await ctx.db.insert("conversationMembers", {
+      memberId: currentUser._id,
+      conversationId,
+    });
+    await ctx.db.insert("conversationMembers", {
+      memberId: request.sender,
+      conversationId,
+    });
     await ctx.db.delete(request._id);
   },
 });
